d3fe7ca8591294665cfdab1cbdf4bccb6548abe2
shyam ramesh <rlshyammdu@gmail.com>
2018-05-11 12:06:19 +0530

    first commit

--- app/src/main/java/helloworld/demo/com/webapplication/MainActivity.java
+++ app/src/main/java/helloworld/demo/com/webapplication/MainActivity.java
@@ -4,19 +4,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView mywebview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView mywebview = (WebView)findViewById(R.id.webview);
+        mywebview = (WebView)findViewById(R.id.webview);
         WebSettings webSettings = mywebview.getSettings();
         webSettings.setJavaScriptEnabled(true);
         mywebview.loadUrl("http://searchurneed.com/");
+        mywebview.setWebViewClient(new WebViewClient());
 
+    }
 
+    @Override
+    public void onBackPressed() {
+        if(mywebview.canGoBack())
+        {
+            mywebview.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 }
